86c200de2ec289ff919c04e78e7db7b2053011c0
Sumedh Shinde <118380339+automationtester23@users.noreply.github.com>
2024-08-27 18:12:33 +0530

    Update LinuxapiApplication.java +7

--- src/main/java/com/sumedh/linuxapi/LinuxapiApplication.java
+++ src/main/java/com/sumedh/linuxapi/LinuxapiApplication.java
@@ -1,5 +1,5 @@
 package com.sumedh.linuxapi;
-//changes done +2
+//changes done +3
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
